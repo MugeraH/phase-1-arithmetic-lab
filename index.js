dd4c0457f79206d81071eb02b06d71dbae8a9a28
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-04-18 14:46:53 +0300

    commit tree 9cc5f1fdd03afc1fe1ae125bea4649a45a889572

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 34
+let num4 =5
+
+let mod = num3%num4
+
+let max = Math.max(1,20)
+
